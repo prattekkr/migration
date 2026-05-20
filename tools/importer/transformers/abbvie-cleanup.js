5c8353a2ebf4fedf890e11752287d8ab81b97f8a
Prattek <pratteks@adobe.com>
2026-05-20 19:06:11 +0530

    removed

--- tools/importer/transformers/abbvie-cleanup.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-export default function transform(hookName, element, payload) {
-  if (hookName !== 'beforeTransform') return;
-  const { document } = payload;
-  ['#onetrust-consent-sdk','.experiencefragment','.cmp-experiencefragment--header','.cmp-experiencefragment--footer','header.nav-bar','.button.back-to-top','link[href]','noscript','script','style','.sticky-nav'].forEach(sel => { document.querySelectorAll(sel).forEach(el => el.remove()); });
-}
